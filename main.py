62e45ec50adb4b1da151a9e57b661fa04bd30461
LachlanDr <lachlan.dreyer@education.nsw.gov.au>
2024-08-27 23:48:20 +1000

    IMPORT FROM MY PC, appending and sequences done

--- main.py
+++ main.py
@@ -1,102 +1,106 @@
-from machine import Pin, PWM, time_pulse_us
+from machine import Pin, PWM
 from time import sleep
 import random
 from piezoNotes import *
 
-#PINS
+# PINS
 green_led = Pin(15, Pin.OUT)
 red_led = Pin(14, Pin.OUT)
 blue_led = Pin(13, Pin.OUT)
 yellow_led = Pin(12, Pin.OUT)
 
-
-#BUTTONS
+# BUTTONS
 green_button = Pin(19, Pin.IN)
 red_button = Pin(18, Pin.IN)
 blue_button = Pin(17, Pin.IN)
 yellow_button = Pin(16, Pin.IN)
 start_button = Pin(20, Pin.IN)
 
-LEDS = (green_led, red_led, blue_led, yellow_led)
+LEDS = [green_led, red_led, blue_led, yellow_led]
+BUTTONS = [green_button, red_button, blue_button, yellow_button]
 
-LIST = []
-#PIEZO 
+# Initialize game sequence
+sequence = []
+user_sequence = []
+game_started = False
+
+# PIEZO
+buzzer = PWM(Pin(11))
+
 def PlayNote(note, duration):
     buzzer.freq(notes[note])
     buzzer.duty_u16(1000)
     sleep(duration)
-    
+    Stop()  
+
 def Stop():
-    buzzer.duty_u16(0)
-    green_led.off()
-    red_led.off()
-    blue_led.off()
-    yellow_led.off()
-
-buzzer = PWM (Pin(11))
-
-bluenoise = PlayNote("E6", 0.4)
-
-    
-buzzer.duty_u16(0)
-def BUTTONTEST():
-    while True:
-        if green_button.value() == True:
-            print ("Button is Pressed")
-            green_led.on()
-            PlayNote("E1", 0.4)
-            
-        if red_button.value() == True:
-            print ("Button is Pressed")
-            red_led.on()
-            PlayNote("F2", 0.4)
-
-        if blue_button.value() == True:
-            blue_led.on()
-            print ("Button is Pressed")
-            PlayNote("E2", 0.4)
-
-        if yellow_button.value() == True:
-            print ("Button is Pressed")
-            yellow_led.on()
-            PlayNote("CS2", 0.4)
-        if start_button.value() == True:
-            print ('ye')
-        
-
-        else:
-            print ("Button is not pressed")
-            green_led.off()
-            red_led.off()
-            blue_led.off()
-            yellow_led.off()
-            Stop()
-
-
-def EVERYTHINGTEST():
-    while True:
-        green_led.on()
-        PlayNote("E1", 0.4)
-        red_led.on()
-        PlayNote("F2", 0.4)
-        blue_led.on()
-        PlayNote("E2", 0.4)
-        yellow_led.on()
-        PlayNote("CS2", 0.4)
-        sleep(5)
-        Stop()
+    buzzer.duty_u16(0)  
+    for led in LEDS:
+        led.off()
 
 def CHOICE():
-
     return random.choice(LEDS)
-def BEGINGAME():
-    led = CHOICE()
-    led.on()
-    LIST.append(f"{led}")
-# yellow_button.value() == True and LEDS == yellow_led:
-#    print("yes")
-if start_button.value() == True:
-    BEGINGAME()
-while True:
-    print (f"{LIST}")
 
+def BEGINGAME():
+    global sequence, user_sequence, game_started
+    Stop()  
+    sequence = []
+    user_sequence = []
+    game_started = True
+
+    AddToSequence()
+
+def AddToSequence():
+    global sequence
+    sequence.append(CHOICE())
+    ShowSequence()
+
+def ShowSequence():
+    Stop()
+    for led in sequence:
+        led.on()
+        PlayNote("E4", 0.4)  
+        sleep(0.4)
+        Stop()  
+        sleep(0.2)
+    
+    GetUserInput()
+
+def GetUserInput():
+    global user_sequence
+    user_sequence = []
+    while len(user_sequence) < len(sequence):
+        for i, button in enumerate(BUTTONS):
+            if button.value() == True:
+                LED = LEDS[i]
+                LED.on()
+                PlayNote("E4", 0.4) 
+                user_sequence.append(LED)
+                sleep(0.4)
+                Stop()
+                sleep(0.2)
+                break
+        sleep(0.1)
+    CheckUserSequence()
+
+def CheckUserSequence():
+    global sequence, user_sequence, game_started
+    if user_sequence == sequence:
+        print("Correct! Adding to the sequence.")
+        AddToSequence()
+    else:
+        print("Incorrect. Game Over!")
+        Stop()
+        game_started = False
+
+def BUTTONTEST():
+    global game_started
+    while True:
+        if start_button.value() == True and not game_started:
+            print('Starting Game')
+            BEGINGAME()
+            sleep(1) 
+
+        sleep(0.1)  
+
+BUTTONTEST()
--- pico.py
+++ pico.py
@@ -1,11 +1,106 @@
-from machine import Pin, ADC
+from machine import Pin, PWM
+from time import sleep
+from piezoNotes import *
+import random
 
-ldr = ADC(Pin(26))
-led1 = (Pin(20))
+# PINS
+green_led = Pin(15, Pin.OUT)
+red_led = Pin(14, Pin.OUT)
+blue_led = Pin(13, Pin.OUT)
+yellow_led = Pin(12, Pin.OUT)
 
-while True:
+# BUTTONS
+green_button = Pin(19, Pin.IN)
+red_button = Pin(18, Pin.IN)
+blue_button = Pin(17, Pin.IN)
+yellow_button = Pin(16, Pin.IN)
+start_button = Pin(20, Pin.IN)
 
-    #read the ldr and print
-    lightValue = ldr.read_u16()
-    print (lightValue)
-    if ldr lightValue 
+LEDS = [green_led, red_led, blue_led, yellow_led]
+BUTTONS = [green_button, red_button, blue_button, yellow_button]
+LED_NAMES = ["green", "red", "blue", "yellow"]
+
+# PIEZO
+def PlayNote(note, duration):
+    buzzer.freq(notes[note])
+    buzzer.duty_u16(1000)
+    sleep(duration)
+    
+def Stop():
+    buzzer.duty_u16(0)
+    for led in LEDS:
+        led.off()
+
+buzzer = PWM(Pin(11))
+buzzer.duty_u16(0)
+
+# Global Variables
+sequence = []
+player_sequence = []
+game_started = False
+
+def show_sequence():
+    for led in sequence:
+        led.on()
+        PlayNote("E1", 0.4)  # You may need to change the note based on LED
+        sleep(0.5)
+        led.off()
+        sleep(0.5)
+
+def get_led_from_button(button):
+    if button == green_button:
+        return green_led
+    elif button == red_button:
+        return red_led
+    elif button == blue_button:
+        return blue_led
+    elif button == yellow_button:
+        return yellow_led
+
+def check_player_input():
+    global player_sequence
+    for led in player_sequence:
+        if not led in sequence:
+            return False
+    return True
+
+def handle_button_press():
+    if start_button.value() == True:
+        start_game()
+
+    for button in BUTTONS:
+        if button.value() == True:
+            led = get_led_from_button(button)
+            led.on()
+            PlayNote("E1", 0.4)
+            player_sequence.append(led)
+            led.off()
+            sleep(0.5)  # Debounce delay
+
+def start_game():
+    global sequence, player_sequence, game_started
+    if not game_started:
+        sequence = []
+        player_sequence = []
+        for _ in range(5):  # Start with a sequence of length 5
+            led = random.choice(LEDS)
+            sequence.append(led)
+        game_started = True
+        show_sequence()
+
+def main():
+    while True:
+        handle_button_press()
+        if len(player_sequence) == len(sequence):
+            if check_player_input():
+                print("Correct sequence!")
+                # Add next step or end game logic
+            else:
+                print("Wrong sequence! Game over.")
+                game_started = False
+                Stop()
+            player_sequence = []
+        sleep(0.1)  # Avoid busy-wait loop
+
+if __name__ == "__main__":
+    main()
