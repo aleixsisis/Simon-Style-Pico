021d8d631756e0c74f04f0c653f66db11df3763e
LachlanDr <lachlan.dreyer@education.nsw.gov.au>
2024-08-29 07:30:39 +1000

    Fix note pitch, game is startable upon button press after loss

--- main.py
+++ main.py
@@ -19,10 +19,19 @@ start_button = Pin(20, Pin.IN)
 LEDS = [green_led, red_led, blue_led, yellow_led]
 BUTTONS = [green_button, red_button, blue_button, yellow_button]
 
+# Define pitches for each color
+color_notes = {
+    green_led: "C5",  
+    red_led: "D4",    
+    blue_led: "E3",   
+    yellow_led: "F2"  
+}
+
 # Initialize game sequence
 sequence = []
 user_sequence = []
 game_started = False
+game_over = False
 
 # PIEZO
 buzzer = PWM(Pin(11))
@@ -42,12 +51,12 @@ def CHOICE():
     return random.choice(LEDS)
 
 def BEGINGAME():
-    global sequence, user_sequence, game_started
+    global sequence, user_sequence, game_started, game_over
     Stop()  
     sequence = []
     user_sequence = []
     game_started = True
-
+    game_over = False
     AddToSequence()
 
 def AddToSequence():
@@ -59,11 +68,10 @@ def ShowSequence():
     Stop()
     for led in sequence:
         led.on()
-        PlayNote("E4", 0.4)  
+        PlayNote(color_notes[led], 0.4)  
         sleep(0.4)
         Stop()  
         sleep(0.2)
-    
     GetUserInput()
 
 def GetUserInput():
@@ -74,7 +82,7 @@ def GetUserInput():
             if button.value() == True:
                 LED = LEDS[i]
                 LED.on()
-                PlayNote("E4", 0.4) 
+                PlayNote(color_notes[LED], 0.4)
                 user_sequence.append(LED)
                 sleep(0.4)
                 Stop()
@@ -82,25 +90,40 @@ def GetUserInput():
                 break
         sleep(0.1)
     CheckUserSequence()
-
+num = 0
 def CheckUserSequence():
-    global sequence, user_sequence, game_started
+    global sequence, user_sequence, game_started, game_over
     if user_sequence == sequence:
-        print("Correct! Adding to the sequence.")
+        print(f"Correct!")
         AddToSequence()
     else:
         print("Incorrect. Game Over!")
+        game_over = True
+        red_led.on()
+        green_led.on()
+        blue_led.on()
+        yellow_led.on()
+        PlayNote("E3", 0.5) 
+        sleep(0.5)
+        Stop()
+        sleep(0.2)
+        red_led.on()
+        green_led.on()
+        blue_led.on()
+        yellow_led.on()
+        PlayNote("E3", 0.5) 
+        sleep(0.5)
         Stop()
         game_started = False
 
 def BUTTONTEST():
-    global game_started
+    global game_started, game_over
     while True:
-        if start_button.value() == True and not game_started:
-            print('Starting Game')
-            BEGINGAME()
-            sleep(1) 
-
+        if start_button.value() == True:
+            if not game_started or game_over:
+                print('Starting Game')
+                BEGINGAME()
+                sleep(1)
         sleep(0.1)  
 
 BUTTONTEST()
